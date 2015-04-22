65884c83d494cab1121ceed557d7b5547785776f
julien <darul75@gmail.com>
2015-04-22 23:07:12 +0200

    l

--- public/bundle.js
+++ public/bundle.js
@@ -20,7 +20,7 @@ var json = require("./chapters-v1.json");
 
 var APP = React.createClass({displayName: "APP",  
   render: function() {
-    var description =  'Le savoir faire d\'Apis Lazuli Consulting consiste à accompagner les RH dans la conduite du changement selon une approche systémique';   
+    var description =  'Le savoir faire d\'Apis Lazuli Consulting consiste à accompagner les RH dans la conduite du changement selon une approche systémique.';   
     return (
       React.createElement("html", null, 
         React.createElement("head", null, 
